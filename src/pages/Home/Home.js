66e4a4c29baf370d75aa30fc2d237ec609a5665c
robonexx <robertwagar@gmail.com>
2021-10-07 20:01:32 +0200

    styling home  page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -15,7 +15,14 @@ const Home = () => {
       </div>
      
       <section className="home-section-top">
-        <h2>a web developer with the goal to make web easier and at your service to solve your problems.</h2>
+        <h2>
+          A creative <b>Frontend developer</b>, with some <br />
+          <small>backend knowledge</small> and then some.<br />
+          Based in Stockholm, Sweden <br />
+          My goal is to make things easier for you,<br />
+          by solving your problems with <b>creative</b> solutions
+        
+        </h2>
       </section>
     </div>
    );
